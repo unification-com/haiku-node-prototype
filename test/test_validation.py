dd369799caada657af73789590bf5a9fdb67e0a1
Codegnosis <paul@codegnosis.co.uk>
2018-05-10 13:59:57 +0100

    clean up test

--- test/test_validation.py
+++ test/test_validation.py
@@ -6,46 +6,46 @@ sys.path.insert(0,parentdir)
 
 from validation.validation import UnificationACLValidation
 
+#get requesting app
 requesting_app = sys.argv[1]
+
+#list of users to test - EOS account names.
+users_to_test = ["user1", "user2", "user3"]
 
 with open(parentdir + '/config/config.json') as f:
     conf = json.load(f)
 
-print("THIS app is",conf['acl_contract'])
+print("THIS app is", conf['acl_contract'])
 
 v = UnificationACLValidation(requesting_app)
 
-print("Requesting App", requesting_app, "Valid accoring to MOTHER: ", v.valid_app())
+app_valid = v.valid_app()
+print("Requesting App", requesting_app, "Valid according to MOTHER: ", app_valid)
 
-users_granted = v.users_granted()
-print("Users who granted permission for:", requesting_app)
-print(users_granted)
+code_valid = v.valid_code()
+print(requesting_app, "contract code hash valid:", code_valid)
 
-users_revoked = v.users_revoked()
-print("Users who revoked permission for:", requesting_app)
-print(users_revoked)
+if app_valid and code_valid:
 
-user = "user1"
-perm = v.app_has_user_permission(user)
-if(perm):
-    print(user, "GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
+    print("App valid according to MOTHER. App code hash valid. Check user permissions.")
+
+    users_granted = v.users_granted()
+    print("Users who granted permission for:", requesting_app)
+    print(users_granted)
+
+    users_revoked = v.users_revoked()
+    print("Users who revoked permission for:", requesting_app)
+    print(users_revoked)
+
+    for user in users_to_test:
+        perm = v.app_has_user_permission(user)
+        if (perm):
+            print(user, "GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
+        else:
+            print(user, "NOT GRANTED permission for ", requesting_app, " to access data in", conf['acl_contract'])
+
 else:
-    print(user, "NOT GRANTED permission for ", requesting_app, " to access data in", conf['acl_contract'])
-
-user = "user2"
-perm = v.app_has_user_permission(user)
-if(perm):
-    print(user, "GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
-else:
-    print(user, "NOT GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
-
-user = "user3"
-perm = v.app_has_user_permission(user)
-if (perm):
-    print(user, "GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
-else:
-    print(user, "NOT GRANTED permission for", requesting_app, "to access data in", conf['acl_contract'])
-
-
-#code_valid = v.check_contract_code_hash("unif1", "a2be684581db0f2ff1b14422c9654004e8229d3f11d0507c7918e54fb613cc68")
-#print("unif1 contract code valid:", code_valid)
+    if app_valid is False:
+        print("App ont valid according to MOTHER")
+    if code_valid is False:
+        print("Code hash did not match hash held by MOTHER")
--- validation/validation.py
+++ validation/validation.py
@@ -18,15 +18,15 @@ class UnificationACLValidation:
         self.requesting_app = requesting_app
         self._call_mother()
         self._get_app_permissions()
+        self._check_acl_contract_code_hash()
 
     def app_has_user_permission(self, user_account):
         has_permission = False
 
         user_acc_uint64 = esoio_account.string_to_name(user_account)
 
-        for i in self.granted:
-            if int(i) == user_acc_uint64:
-                has_permission = True
+        if user_acc_uint64 in self.granted:
+            has_permission = True
 
         return has_permission
 
@@ -39,22 +39,30 @@ class UnificationACLValidation:
     def valid_app(self):
         return self.is_valid_app
 
-    def check_acl_contract_code_hash(self, hash):
-        code_valid = False
+    def valid_code(self):
+        return self.is_valid_code
+
+    def _check_acl_contract_code_hash(self):
+        self.is_valid_code = False
         json_data = self.eosClient.get_code(self.requesting_app)
         code_hash = json_data['code_hash']
-        if(hash == code_hash):
-            code_valid = True
 
-        return code_valid
+        req_app_uint64 = esoio_account.string_to_name(self.requesting_app)
+
+        mother_data = self.eosClient.get_table_rows("unif.mother", "unif.mother", "validapps", True, 0, -1, -1)
+
+        for i in mother_data['rows']:
+            if int(i['acl_contract_acc']) == req_app_uint64 and i['acl_contract_hash'] == code_hash:
+                self.is_valid_code = True
 
     def _get_app_permissions(self):
         self.granted = []
         self.revoked = []
-        table_data = self.eosClient.get_table_rows(self.requesting_app, self.conf['acl_contract'], "unifacl", True, 0, -1, -1)
+        table_data = self.eosClient.get_table_rows(self.requesting_app, self.conf['acl_contract'], "unifacl", True, 0,
+                                                   -1, -1)
 
         for i in table_data['rows']:
-            if(int(i['permission_granted']) == 1):
+            if int(i['permission_granted']) == 1:
                 self.granted.append(int(i['user_account']))
             else:
                 self.revoked.append(int(i['user_account']))
