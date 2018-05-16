97e3459fc461207f851f1a54716a657caeef67a8
Indika Piyasena <indika.piyasena@gmail.com>
2018-05-16 22:40:17 +1000

    main.py: calls the RPC client if a password is provided

--- haiku_node/main.py
+++ haiku_node/main.py
@@ -1,9 +1,14 @@
 import sys
 
 from cryptography.fernet import Fernet
+from pathlib import Path
 
 from haiku_node.config.config import UnificationConfig
 from haiku_node.keystore.keystore import UnificationKeystore
+from haiku_node.rpc import app
+
+# A plain text on the file-system password to ease automated systems
+HAIKU_PASSWORD_FILE = Path('/etc/haiku-password')
 
 
 def spawn_haiku_node(pw, conf):
@@ -28,22 +33,31 @@ def spawn_haiku_node(pw, conf):
     print("Private auth key: ", ks.get_rpc_auth_private_key())
     print("Public auth key: ", ks.get_rpc_auth_public_key())
 
+    app.run(debug=False, host="0.0.0.0", port=8050)
+
 
 if __name__ == '__main__':
     config = UnificationConfig()
     conf = config.get_conf()
-    if 'server_initialised' in conf:
-        if len(sys.argv) > 1:
-            pw = sys.argv[1]
-            spawn_haiku_node(pw, conf)
-        else:
-            print("password required")
-    else:
-        pw = Fernet.generate_key()
-        print("Generated password:\n")
-        sys.stdout.buffer.write(pw)
-        print("\n")
-        print("IMPORTANT: KEEP THIS SAFE!! YOU WILL NEED IT TO RUN THE SERVER")
-        print("Run again with pw")
 
-        config.set_conf("server_initialised", True)
+    if HAIKU_PASSWORD_FILE.exists():
+        password = HAIKU_PASSWORD_FILE.read_text()
+        spawn_haiku_node(password, conf)
+
+    else:
+        if 'server_initialised' in conf:
+            if len(sys.argv) > 1:
+                pw = sys.argv[1]
+                spawn_haiku_node(pw, conf)
+            else:
+                print("password required")
+        else:
+            pw = Fernet.generate_key()
+            print("Generated password:\n")
+            sys.stdout.buffer.write(pw)
+            print("\n")
+            print("IMPORTANT: KEEP THIS SAFE!! "
+                  "YOU WILL NEED IT TO RUN THE SERVER")
+            print("Run again with pw")
+
+            config.set_conf("server_initialised", True)
