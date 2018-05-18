0b400369bedef83d8d737236b3088bfaa6d5577a
Indika Piyasena <indika.piyasena@gmail.com>
2018-05-18 17:34:13 +1000

    refactor so we can re-use code

--- haiku_node/rpc.py
+++ haiku_node/rpc.py
@@ -1,5 +1,7 @@
 import flask
 
+from cryptography.exceptions import InvalidSignature
+
 from haiku_node.config.keys import get_public_key
 from haiku_node.validation.encryption import verify_request, sign_request
 
@@ -9,14 +11,8 @@ app.logger_name = "haiku-rpc"
 logger = app.logger
 
 
-def verify_account(eos_account_name, body, signature) -> bool:
-    logger.info(f"Attempting to authorize {eos_account_name}")
-    return verify_request(get_public_key(eos_account_name), body, signature)
-
-
-def obtain_data(body):
-    data = 'DATA'
-    return data
+def verify_account(eos_account_name, body, signature):
+    verify_request(get_public_key(eos_account_name), body, signature)
 
 
 def sign_data(body):
@@ -25,26 +21,56 @@ def sign_data(body):
     return sign_request(private_key, body)
 
 
+def invalid_response():
+    return flask.jsonify({
+        'success': False,
+        'message': 'Unauthorized',
+        'signature': None,
+        'body': None
+    }), 401
+
+
+def obtain_data(body):
+    data = 'DATA'
+    return flask.jsonify({
+        'success': True,
+        'message': 'Success',
+        'signature': sign_data(data),
+        'body': data
+    }), 200
+
+
+def ingest_data(body):
+    #TODO: The response body needs to be signed before jsonified
+    response_body = "body"
+    return flask.jsonify({
+        'success': True,
+        'message': 'Success',
+        'signature': sign_data(response_body),
+        'body': response_body
+    }), 200
+
+
 @app.route('/data_request', methods=['POST'])
 def data_request():
-    d = flask.request.get_json()
+    try:
+        d = flask.request.get_json()
+        verify_account(d['eos_account_name'], d['body'], d['signature'])
+        return obtain_data(d['body'])
 
-    if not verify_account(d['eos_account_name'], d['body'], d['signature']):
-        return flask.jsonify({
-                'success': False,
-                'message': 'Unauthorized',
-                'signature': None,
-                'body': None
-            }), 401
+    except InvalidSignature:
+        return invalid_response()
 
-    data = obtain_data(d['body'])
 
-    return flask.jsonify({
-            'success': True,
-            'message': 'Success',
-            'signature': sign_data(data),
-            'body': data
-        }), 200
+@app.route('/data_ingest', methods=['POST'])
+def data_ingest():
+    try:
+        d = flask.request.get_json()
+        verify_account(d['eos_account_name'], d['body'], d['signature'])
+        return ingest_data(d['body'])
+
+    except InvalidSignature:
+        return invalid_response()
 
 
 if __name__ == '__main__':
