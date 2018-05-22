170d447e31b66c6286afa35c93809998828de2a4
Indika Piyasena <indika.piyasena@gmail.com>
2018-05-22 20:57:52 +1000

    test_asymmetric: except the InvalidSignature exception

--- test/test_asymmetric.py
+++ test/test_asymmetric.py
@@ -1,3 +1,5 @@
+import pytest
+
 from haiku_node.config.keys import get_public_key, get_private_key
 from haiku_node.validation.encryption import sign_request, verify_request
 
@@ -14,8 +16,11 @@ def test_sign_and_verify():
 
     broken_signature = 'unlucky' + signature[7:]
 
-    assert verify_request(public_key, message, broken_signature) is False
-    assert verify_request(public_key, message, signature) is True
+    from cryptography.exceptions import InvalidSignature
+    with pytest.raises(InvalidSignature):
+        verify_request(public_key, message, broken_signature)
+
+    verify_request(public_key, message, signature)
 
 
 def test_get_public_key():
