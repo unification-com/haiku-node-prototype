06d969a54a9e8db0b0ff04a7896dc161fa41b52f
Indika Piyasena <indika.piyasena@gmail.com>
2018-05-22 20:59:45 +1000

    validation.encryption: remove used import

--- haiku_node/validation/encryption.py
+++ haiku_node/validation/encryption.py
@@ -1,6 +1,5 @@
 import base64
 
-from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
