3eafe52a4f7913281e9e359c87dfc7ebf0af27a2
Indika Piyasena <indika.piyasena@gmail.com>
2018-05-25 10:40:23 +1000

    blockchain.acl: pep8, and use compacted dictionary creation

--- haiku_node/blockchain/acl.py
+++ haiku_node/blockchain/acl.py
@@ -6,26 +6,21 @@ class UnificationACL:
     Loads data from ACL/Meta Smart Contract for a Haiku node's app
     """
 
-    def __init__(self,
-                 eos_rpc_ip,
-                 eos_rpc_port,
-                 acl_contract_acc):
-
+    def __init__(self, eos_rpc_ip, eos_rpc_port, acl_contract_acc):
         """
-            :param eos_rpc_ip: IP for EOS Client to communicate with the blockchain
-            :param eos_rpc_port: Port for EOS Client to communicate with the blockchain
-            :param acl_contract_acc: the eos account name of the app for which the class will retrieve
-                           data from the ACL/Meta Data smart contract
+        :param eos_rpc_ip: IP for EOS Client to communicate with the
+            blockchain
+        :param eos_rpc_port: Port for EOS Client to communicate with the
+            blockchain
+        :param acl_contract_acc: the eos account name of the app for which the
+            class will retrieve data from the ACL/Meta Data smart contract
         """
-
         self.__permission_rec_table = "permrecords"
         self.__db_schema_table = "dataschemas"
         self.__data_sources_table = "datasources"
 
         self.__acl_contract_acc = acl_contract_acc
-        self.__eosClient = Client(
-            nodes=[f"http://{eos_rpc_ip}"
-                   f":{eos_rpc_port}"])
+        self.__eosClient = Client(nodes=[f"http://{eos_rpc_ip}:{eos_rpc_port}"])
 
         self.__db_schemas = {}
         self.__data_sources = {}
@@ -48,7 +43,8 @@ class UnificationACL:
         # TODO: run in loop and check JSON result for "more" value
         table_data = self.__eosClient.get_table_rows(
             requesting_app,
-            self.__acl_contract_acc, self.__permission_rec_table, True, 0, -1, -1)
+            self.__acl_contract_acc, self.__permission_rec_table, True, 0, -1,
+            -1)
 
         granted = []
         revoked = []
@@ -68,12 +64,13 @@ class UnificationACL:
 
         for i in table_data['rows']:
             key = f'{i["schema_name_str"]}.{i["schema_vers"]}'
-            d = {}
-            d['schema_id'] = i['pkey']
-            d['schema_name'] = i['schema_name']
-            d['schema_name_str'] = i['schema_name_str']
-            d['schema_vers'] = i['schema_vers']
-            d['schema'] = i['schema']
+            d = {
+                'schema_id': i['pkey'],
+                'schema_name': i['schema_name'],
+                'schema_name_str': i['schema_name_str'],
+                'schema_vers': i['schema_vers'],
+                'schema': i['schema']
+            }
             self.__db_schemas[key] = d
 
     def __load_data_sources(self):
@@ -83,14 +80,15 @@ class UnificationACL:
 
         for i in table_data['rows']:
             source_name = i['source_name']
-            d = {}
-            d['pkey'] = i['pkey']
-            d['source_name'] = i['source_name']
-            d['source_name_str'] = i['source_name_str']
-            d['source_type'] = i['source_type']
-            d['schema_id'] = i['schema_id']
-            d['acl_contract_acc'] = i['acl_contract_acc']
-            d['in_use'] = i['in_use']
+            d = {
+                'pkey': i['pkey'],
+                'source_name': i['source_name'],
+                'source_name_str': i['source_name_str'],
+                'source_type': i['source_type'],
+                'schema_id': i['schema_id'],
+                'acl_contract_acc': i['acl_contract_acc'],
+                'in_use': i['in_use']
+            }
             self.__data_sources[source_name] = d
 
     def __run(self):
