10258a9bf37988f6774f64da4ddb1de5e4ecafaf
Codegnosis <paul@codegnosis.co.uk>
2018-06-01 12:13:32 +0100

    remove registered_users from config.py - no longer required. Data factory handles user list, byt acquiring from ACL contract

--- haiku_node/config/config.py
+++ haiku_node/config/config.py
@@ -10,9 +10,6 @@ log = logging.getLogger(__name__)
 class UnificationConfig:
     config_path = '/config/config.json'
 
-    # Haiku server needs registered users to support bulk ingestion
-    registered_users = ['user1', 'user2', 'user3']
-
     def __init__(self):
         with open(self.parent_directory() + self.config_path) as f:
             self.__conf = json.load(f)
