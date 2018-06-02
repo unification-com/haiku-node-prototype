7096ffba1b91dce0392f10794724efd1b429212e
Indika Piyasena <indika.piyasena@gmail.com>
2018-06-02 15:14:17 +1000

    data.connector: app_connector provides the relevant engine

--- haiku_node/data/connector.py
+++ haiku_node/data/connector.py
@@ -1,30 +1,6 @@
 from sqlalchemy import create_engine
 
-heartbit_data_source_parms = {
-    'odbc': 'mysql+mysqlconnector',
-    'database': 'Heartbit',
-    'host': 'db',
-    'user': 'heartbit_user',
-    'pass': 'password',
-    'userTable': 'Users',
-    'dataTable': 'UserData',
-    'userIdentifier': 'ID',
-    'dataUserIdentifier': 'UserID',
-    'dataColumnsToInclude': ['Heartrate', 'GeoLocation', 'TimeStamp', 'Pulse']
-}
-
-blobdata_data_source_parms = {
-    'odbc': 'mysql+mysqlconnector',
-    'database': 'Datablobs',
-    'host': 'db',
-    'user': 'datablobs_user',
-    'pass': 'password',
-    'userTable': 'BlobCreator',
-    'dataTable': 'BlobData',
-    'userIdentifier': 'CreatorID',
-    'dataUserIdentifier': 'CreatorID',
-    'dataColumnsToInclude': ['DataBlob', 'BlobSize']
-}
+from haiku_node.config.config import UnificationConfig
 
 
 def assemble_connection_string(data_source):
@@ -34,25 +10,35 @@ def assemble_connection_string(data_source):
     host = data_source['host']
     database = data_source['database']
 
-    # TODO: The datasource should have the port
+    # TODO: The data source should have the port
     port = '3306'
     return f'{odbc}://{user}:{passw}@{host}:{port}/{database}'
 
 
-def excercise_connection(source, query):
-    conn_string = assemble_connection_string(source)
+def app_connector(app_config):
+    params = app_config['data_source_params']
+    conn_string = assemble_connection_string(params)
     engine = create_engine(conn_string)
+    return engine
+
+
+def run_query(engine, query):
     t = engine.execute(query)
     result = [r[0] for r in t]
     print(result)
 
 
-def process():
-    excercise_connection(
-        heartbit_data_source_parms, "SELECT FirstName FROM Users;")
-    excercise_connection(
-        blobdata_data_source_parms, "SELECT Name FROM BlobCreator;")
+def evaluate():
+    config = UnificationConfig()
+    app_config = config.demo_app_config()
+    engine = app_connector(app_config)
+
+    if app_config['data_source_params']['database'] == 'Heartbit':
+        run_query(engine, "SELECT FirstName FROM Users;")
+
+    if app_config['data_source_params']['database'] == 'Datablobs':
+        run_query(engine, "SELECT Name FROM BlobCreator;")
 
 
 if __name__ == "__main__":
-    process()
+    evaluate()
