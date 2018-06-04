4b4a302826435b29422b9e15968b597eb1a52c14
Indika Piyasena <indika.piyasena@gmail.com>
2018-06-04 22:52:35 +1000

    data.connector: evaluate can provide a sample string

--- haiku_node/data/connector.py
+++ haiku_node/data/connector.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from haiku_node.config.config import UnificationConfig
 from haiku_node.data.transform_data import fetch_user_data
 
+
 def assemble_connection_string(data_source):
     odbc = data_source['odbc']
     user = data_source['user']
@@ -15,33 +16,34 @@ def assemble_connection_string(data_source):
     return f'{odbc}://{user}:{passw}@{host}:{port}/{database}'
 
 
-def app_connector(app_config):
-    params = app_config['data_source_params']
-    conn_string = assemble_connection_string(params)
-    engine = create_engine(conn_string)
-    return engine
+def app_connector(data_source_params):
+    conn_string = assemble_connection_string(data_source_params)
+    return create_engine(conn_string)
 
 
 def run_query(engine, query):
     t = engine.execute(query)
     result = [r[0] for r in t]
-    print(result)
+    return str(result)
 
 
-def evaluate():
+def evaluate(data_source_params):
+    engine = app_connector(data_source_params)
+
+    if data_source_params['database'] == 'Heartbit':
+        return run_query(engine, "SELECT FirstName FROM Users;")
+
+    if data_source_params['database'] == 'Datablobs':
+        return run_query(engine, "SELECT Name FROM BlobCreator;")
+
+
+if __name__ == "__main__":
+    """
+    Testing fetch_user_data
+    """
     config = UnificationConfig()
     app_config = config.demo_app_config()
     engine = app_connector(app_config)
 
     res = fetch_user_data(app_config['data_source_params'])
     print(res)
-    
-    if app_config['data_source_params']['database'] == 'Heartbit':
-        run_query(engine, "SELECT FirstName FROM Users;")
-
-    if app_config['data_source_params']['database'] == 'Datablobs':
-        run_query(engine, "SELECT Name FROM BlobCreator;")
-
-
-if __name__ == "__main__":
-    evaluate()
