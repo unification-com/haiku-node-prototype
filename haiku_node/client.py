9db78b483fecbb11a66bf55495f2be2bfec11ab6
Codegnosis <paul@codegnosis.co.uk>
2018-06-11 17:07:29 +0100

    initial UND reward transfer hooks

--- haiku_node/client.py
+++ haiku_node/client.py
@@ -1,12 +1,15 @@
 import json
 import tempfile
+import subprocess
+import xml.etree.ElementTree as etree
 from pathlib import Path
 
 import logging
-
 import requests
 
 from haiku_node.encryption.payload import bundle, unbundle
+from haiku_node.config.config import UnificationConfig
+from eosapi import Client
 
 log = logging.getLogger(__name__)
 
@@ -31,6 +34,8 @@ class HaikuDataClient:
         self.keystore = keystore
         self.local = local
         self.protocol = protocol
+        self.__unif_config = UnificationConfig()
+        self.__my_acc_name = self.__unif_config["acl_contract"]
 
     def transform_request_id(self, user, request_hash):
         """
@@ -72,6 +77,14 @@ class HaikuDataClient:
 
         log.info(f'"In the air" decrypted content is: {decrypted_body}')
 
+        #TODO: better method
+        tree = etree.ElementTree(etree.fromstring(decrypted_body))
+        users_to_pay = tree.findall('unification_users/unification_user')
+        for username in users_to_pay:
+            print(f'pay {username.text}')
+            ret = self.__transfer_und(username.text)
+            print(ret)
+
         return self.persist_data(
             providing_app.name, request_hash, d)
 
@@ -89,3 +102,35 @@ class HaikuDataClient:
 
         log.info(f'Decrypted content from persistence store: {decrypted_body}')
         return decrypted_body
+
+    # TODO: move somewhere else, and improve!
+    def __transfer_und(self, username):
+
+        pre = ["/opt/eosio/bin/cleos", "--url", "http://nodeosd:8888",
+               "--wallet-url", 'http://keosd:8889']
+
+        d = {
+            'from': self.__my_acc_name,
+            'to': username,
+            'quantity': '1.0000 UND',
+            'memo': 'UND Reward'
+        }
+        # cleos push action eosio.token transfer '[ "app3", "user1", "1.0000 UND", "m" ]' -p app3
+        subcommands = ["push", "action", "unif.token", "transfer", json.dumps(d), "-p", self.__my_acc_name]
+
+        cmd = pre + subcommands
+
+        log.debug(f"cleos command: {cmd}")
+
+        result = subprocess.run(
+            cmd, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, universal_newlines=True)
+
+        log.debug(result.stdout)
+        if result.returncode != 0:
+            log.warning(result.stdout)
+            log.debug(result.stdout)
+            log.debug(result.stderr)
+            log.debug(' '.join(cmd))
+
+        return result
