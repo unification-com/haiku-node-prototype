40992f7f2b4fbfde442ee0c0f460812dd645da6c
Indika Piyasena <indika.piyasena@gmail.com>
2018-07-07 10:28:32 +0530

    test: easy access to keys should only be availiable for tests

--- test/test_asymmetric.py
+++ test/test_asymmetric.py
@@ -1,19 +1,52 @@
 # encoding: utf-8
 
 import json
-
+import os
 import pytest
 
 from cryptography.exceptions import InvalidSignature
 from cryptography.fernet import InvalidToken
 
-from haiku_node.config.keys import get_public_key, get_private_key
+from haiku_node.encryption.payload import bundle, unbundle
 from haiku_node.encryption.tools import (
     symmetric_encrypt, symmetric_decrypt, sign_request, verify_request)
-from haiku_node.encryption.payload import bundle, unbundle
+from haiku_node.keystore.keystore import UnificationKeystore
+
+from unittest.mock import patch
+from pathlib import Path
+
+
+def password_for_app(app_name):
+    """
+    The key stores currently do not contain the public keys of the peer apps
+    """
+    current_directory = Path(os.path.abspath(__file__))
+    par = current_directory.parent.parent
+    config = par / Path('test/data/demo_config.json')
+
+    contents = config.read_text()
+    d = json.loads(contents)["system"]
+
+    return d[app_name]['password']
+
+
+def mock_get_private_key(app_name):
+    encoded_password = str.encode(password_for_app(app_name))
+    ks = UnificationKeystore(encoded_password, app_name=app_name)
+    return ks.get_rpc_auth_private_key()
+
+
+def mock_get_public_key(app_name):
+    encoded_password = str.encode(password_for_app(app_name))
+
+    ks = UnificationKeystore(encoded_password, app_name=app_name)
+    return ks.get_rpc_auth_public_key()
 
 
 def test_get_public_key():
+    get_public_key = mock_get_public_key
+    get_private_key = mock_get_private_key
+
     assert get_public_key('app2') is not None
     assert get_private_key('app2') is not None
 
@@ -38,6 +71,9 @@ def test_symmetric():
     "'¡ooʇ ןnɟǝsn sı uʍop-ǝpısdn unicode ¡ooʇ ןnɟǝsn sı uʍop-ǝpısdn"
 ])
 def test_sign_and_verify(message):
+    get_public_key = mock_get_public_key
+    get_private_key = mock_get_private_key
+
     private_key = get_private_key('app2')
     public_key = get_public_key('app2')
 
@@ -66,18 +102,22 @@ def test_transfer_over_json(payload_d):
         d[field] = 'unlucky' + d[field][7:]
         return d
 
-    sender = 'app1'
-    recipient = 'app2'
+    with patch('haiku_node.encryption.payload.get_public_key',
+               mock_get_public_key):
+        sender = 'app1'
+        recipient = 'app2'
 
-    message = bundle(sender, recipient, payload_d, 'Success')
-    wire_data = json.dumps(message)
+        message = bundle(sender, recipient, payload_d, 'Success')
+        wire_data = json.dumps(message)
 
-    with pytest.raises(InvalidToken):
-        unbundle(sender, recipient, broken(json.loads(wire_data), 'payload'))
+        with pytest.raises(InvalidToken):
+            unbundle(
+                sender, recipient, broken(json.loads(wire_data), 'payload'))
 
-    with pytest.raises(InvalidSignature):
-        unbundle(sender, recipient, broken(json.loads(wire_data), 'signature'))
+        with pytest.raises(InvalidSignature):
+            unbundle(
+                sender, recipient, broken(json.loads(wire_data), 'signature'))
 
-    reload = json.loads(wire_data)
-    reload_d = unbundle(sender, recipient, reload)
-    assert payload_d == reload_d
+        reload = json.loads(wire_data)
+        reload_d = unbundle(sender, recipient, reload)
+        assert payload_d == reload_d
