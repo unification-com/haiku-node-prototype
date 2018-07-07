5c44be33bb8d648e821aabc3fa579029d72ce480
Indika Piyasena <indika.piyasena@gmail.com>
2018-07-07 10:55:00 +0530

    keys.py: get the public key from IPFS

--- haiku_node/config/keys.py
+++ haiku_node/config/keys.py
@@ -3,6 +3,14 @@ import os
 
 from pathlib import Path
 
+from eosapi import Client
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+from haiku_node.blockchain.acl import UnificationACL
+from haiku_node.blockchain.ipfs import IPFSDataStore
+from haiku_node.config.config import UnificationConfig
 from haiku_node.keystore.keystore import UnificationKeystore
 
 
@@ -21,9 +29,18 @@ def password_for_app(app_name):
 
 
 def get_public_key(app_name):
-    encoded_password = str.encode(password_for_app(app_name))
-    ks = UnificationKeystore(encoded_password, app_name=app_name)
-    return ks.get_rpc_auth_public_key()
+    config = UnificationConfig()
+    eos_client = Client(
+        nodes=[f"http://{config['eos_rpc_ip']}:{config['eos_rpc_port']}"])
+    u_acl = UnificationACL(eos_client, app_name)
+    public_key_hash = u_acl.get_public_key_hash(app_name)
+
+    store = IPFSDataStore()
+    public_key = store.cat_file(public_key_hash)
+
+    return serialization.load_pem_public_key(
+        public_key,
+        backend=default_backend())
 
 
 def get_private_key(app_name):
