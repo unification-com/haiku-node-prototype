de9f7d1271c673e2fa1a8597a7edd6d57c54140d
Indika Piyasena <indika.piyasena@gmail.com>
2018-07-09 13:44:24 +0530

    config.keys: remove methods that magically provide keys

--- haiku_node/config/keys.py
+++ haiku_node/config/keys.py
@@ -1,8 +1,3 @@
-import json
-import os
-
-from pathlib import Path
-
 from eosapi import Client
 
 from cryptography.hazmat.backends import default_backend
@@ -11,21 +6,6 @@ from cryptography.hazmat.primitives import serialization
 from haiku_node.blockchain.acl import UnificationACL
 from haiku_node.blockchain.ipfs import IPFSDataStore
 from haiku_node.config.config import UnificationConfig
-from haiku_node.keystore.keystore import UnificationKeystore
-
-
-def password_for_app(app_name):
-    """
-    The key stores currently do not contain the public keys of the peer apps
-    """
-    current_directory = Path(os.path.abspath(__file__))
-    par = current_directory.parent.parent.parent
-    config = par / Path('test/data/demo_config.json')
-
-    contents = config.read_text()
-    d = json.loads(contents)["system"]
-
-    return d[app_name]['password']
 
 
 def get_public_key(app_name):
@@ -41,9 +21,3 @@ def get_public_key(app_name):
     return serialization.load_pem_public_key(
         public_key,
         backend=default_backend())
-
-
-def get_private_key(app_name):
-    encoded_password = str.encode(password_for_app(app_name))
-    ks = UnificationKeystore(encoded_password, app_name=app_name)
-    return ks.get_rpc_auth_private_key()
