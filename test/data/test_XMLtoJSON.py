5cde256800d68960397f699149cc08da22491259
Indika Piyasena <indika.piyasena@gmail.com>
2018-09-02 18:41:21 +0200

    prototype.test: pytest automatically finds aptly named tests

--- test/data/test_XMLtoJSON.py
+++ test/data/test_XMLtoJSON.py
@@ -1,5 +1,6 @@
 import json
 
+import pytest
 import xmljson
 
 from lxml.etree import fromstring, tostring
@@ -9,12 +10,10 @@ schema_2 = "<schema-template><fields><field><name>account_name</name><type>varch
 schema_3 = "<schema-template><fields><field><name>account_name</name><type>varchar</type><is-null>false</is-null><table>unification_lookup</table></field><field><name>Image</name><type>base64_mime_image</type><is-null>true</is-null><table>data_1</table></field></fields></schema-template>"
 
 
-def xml_to_json(xml_str):
+@pytest.mark.parametrize("xml_str", [schema_1, schema_2, schema_3])
+def test_xml_to_json(xml_str):
     xml = fromstring(xml_str)
     json_str = json.dumps(xmljson.gdata.data(xml))
     d = json.loads(json_str)
     print(json_str)
     print(d)
-
-
-xml_to_json(schema_1)
