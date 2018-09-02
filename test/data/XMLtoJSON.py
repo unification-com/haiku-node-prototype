c337d6156323f76db9922b9394d6d23b1d565cd6
Indika Piyasena <indika.piyasena@gmail.com>
2018-09-02 18:29:16 +0200

    prototype.test: one import per line

--- test/data/XMLtoJSON.py
+++ test/data/XMLtoJSON.py
@@ -1,4 +1,7 @@
-import json, xmljson
+import json
+
+import xmljson
+
 from lxml.etree import fromstring, tostring
 
 schema_1 = "<schema-template><fields><field><name>account_name</name><type>varchar</type><is-null>false</is-null><table>unification_lookup</table></field><field><name>Heartrate</name><type>int</type><is-null>true</is-null><table>data_1</table></field><field><name>GeoLocation</name><type>int</type><is-null>true</is-null><table>data_1</table></field><field><name>TimeStamp</name><type>int</type><is-null>true</is-null><table>data_1</table></field><field><name>Pulse</name><type>int</type><is-null>true</is-null><table>data_1</table></field></fields></schema-template>"
