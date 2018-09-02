cacc9b0c7c565c2e00d61759829f07ec0415bad7
Indika Piyasena <indika.piyasena@gmail.com>
2018-09-02 18:28:21 +0200

    prototype.test: input is a built-in name

--- test/data/XMLtoJSON.py
+++ test/data/XMLtoJSON.py
@@ -6,8 +6,8 @@ schema_2 = "<schema-template><fields><field><name>account_name</name><type>varch
 schema_3 = "<schema-template><fields><field><name>account_name</name><type>varchar</type><is-null>false</is-null><table>unification_lookup</table></field><field><name>Image</name><type>base64_mime_image</type><is-null>true</is-null><table>data_1</table></field></fields></schema-template>"
 
 
-def xml_to_json(input):
-    xml = fromstring(input)
+def xml_to_json(xml_str):
+    xml = fromstring(xml_str)
     json_str = json.dumps(xmljson.gdata.data(xml))     #this is a string
     data = json.loads(json_str)                        #this is a dictionary
     print(json_str)
