5f3d3d73c4fbc294c4b5035a2c7b245b17a4fc93
Indika Piyasena <indika.piyasena@gmail.com>
2018-09-02 18:24:29 +0200

    prototype.test: use appropriate spacing

--- test/data/XMLtoJSON.py
+++ test/data/XMLtoJSON.py
@@ -5,11 +5,13 @@ schema_1 = "<schema-template><fields><field><name>account_name</name><type>varch
 schema_2 = "<schema-template><fields><field><name>account_name</name><type>varchar</type><is-null>false</is-null><table>unification_lookup</table></field><field><name>DataBlob</name><type>binarydata</type><is-null>true</is-null><table>data_1</table></field><field><name>BlobSize</name><type>int</type><is-null>true</is-null><table>data_1</table></field></fields></schema-template>"
 schema_3 = "<schema-template><fields><field><name>account_name</name><type>varchar</type><is-null>false</is-null><table>unification_lookup</table></field><field><name>Image</name><type>base64_mime_image</type><is-null>true</is-null><table>data_1</table></field></fields></schema-template>"
 
+
 def XMLtoJSON(input):
     xml = fromstring(input)
     JSON = json.dumps(xmljson.gdata.data(xml))     #this is a string
     data = json.loads(JSON)                        #this is a dictionary
     print(JSON)
     print(data)
+
 
 XMLtoJSON(schema_1)
