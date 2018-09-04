ebde29e8654907cab5a6cc13ac33582285a851d6
Codegnosis <paul@codegnosis.co.uk>
2018-09-04 17:13:15 +0100

    migrate keys.py to new UApp SC

--- haiku_node/config/keys.py
+++ haiku_node/config/keys.py
@@ -3,7 +3,7 @@ from eosapi import Client
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 
-from haiku_node.blockchain.acl import UnificationACL
+from haiku_node.blockchain.uapp import UnificationUapp
 from haiku_node.blockchain.ipfs import IPFSDataStore
 from haiku_node.config.config import UnificationConfig
 
@@ -12,8 +12,8 @@ def get_public_key(app_name):
     config = UnificationConfig()
     eos_client = Client(
         nodes=[f"http://{config['eos_rpc_ip']}:{config['eos_rpc_port']}"])
-    u_acl = UnificationACL(eos_client, app_name)
-    public_key_hash = u_acl.get_public_key_hash(app_name)
+    uapp_sc = UnificationUapp(eos_client, app_name)
+    public_key_hash = uapp_sc.get_public_key_hash(app_name)
 
     store = IPFSDataStore()
     public_key = store.cat_file(public_key_hash)
