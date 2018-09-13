c4a271285e38fc569575c7f10d8db6bc17e05266
Codegnosis <paul@codegnosis.co.uk>
2018-09-13 15:44:01 +0100

    wait for background processes to finish in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,15 @@
 
 export HAIKU_ROOT=$(pwd)
 
-docker-compose --file Docker/docker-compose.yml down --remove-orphans
-make
+echo "Clean  Docker orphans"
+
+docker-compose --file Docker/docker-compose.yml down --remove-orphans &
+PID=$!
+wait $PID
+
+echo "Run make"
+make &
+PID=$!
+wait $PID
+
 docker-compose --file Docker/docker-compose.yml up
