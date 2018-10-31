1c1a0ae47d0394651dfc8c1709223cf7c240e03a
Codegnosis <paul@codegnosis.co.uk>
2018-10-31 13:48:13 +0000

    add payload tampering to JWT Unit test

--- test/test_jwt.py
+++ test/test_jwt.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import pytest
 
 from haiku_node.blockchain_helpers.eos.eos_keys import UnifEosKey
@@ -23,6 +25,22 @@ def generate_jwt(payload, private_key):
     jwt = unif_jwt.to_jwt()
 
     return jwt
+
+
+def base64url_decode(input_str):
+    rem = len(input_str) % 4
+    if rem > 0:
+        input_str += b'=' * (4 - rem)
+
+    return base64.urlsafe_b64decode(input_str)
+
+
+def base64url_encode(input_str):
+    return base64.urlsafe_b64encode(input_str).replace(b'=', b'')
+
+
+def json_encode(input_str):
+    return json.dumps(input_str, separators=(',', ':')).encode('utf-8')
 
 
 @pytest.mark.parametrize("payload", [
@@ -74,4 +92,39 @@ def test_jwt_key_mismatch(payload):
     pl = unif_jwt.decode_jwt(public_key2)
 
     # should fail - key mismatch (decoded payload should be empty)
+    assert not pl is True
+
+
+@pytest.mark.parametrize("payload", [
+    {
+        "user": "user2",
+        "foo": "bar"
+    },
+    {
+        "x": "y",
+        "z": "a"
+    },
+    {
+        "dfg": "123",
+        "hjk": "456"
+    }
+])
+def test_jwt_payload_mismatch(payload):
+
+    private_key, public_key = generate_eos_key_pair()
+    jwt = generate_jwt(payload, private_key)
+
+    # tamper with payload
+    jwt_list = jwt.split('.')
+    tampered_payload = json.loads(base64url_decode(jwt_list[1].encode('utf-8')))
+    tampered_payload['nonce'] = 12345
+
+    # repackage JWT
+    jwt_tampered_payload_enc = base64url_encode(json_encode(tampered_payload))
+    tampered_jwt = jwt_list[0] + "." + str(jwt_tampered_payload_enc.decode()) + "." + jwt_list[2]
+
+    unif_jwt = UnifJWT(tampered_jwt)
+    pl = unif_jwt.decode_jwt(public_key)
+
+    # should fail - tampered payload (decoded payload should be empty)
     assert not pl is True
