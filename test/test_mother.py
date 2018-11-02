c3e45f731674327743907f7c302847339e1a8a0d
Indika Piyasena <indika.piyasena@gmail.com>
2018-11-02 18:05:56 +0300

    test: use the get_eos_rpc_client wrapper

--- test/test_mother.py
+++ test/test_mother.py
@@ -1,11 +1,9 @@
 import json
 from pathlib import Path
 
-from eosapi import Client
-
 from haiku_node.blockchain.eos.mother import UnificationMother
 from haiku_node.config.config import UnificationConfig
-from haiku_node.network.eos import get_cleos
+from haiku_node.network.eos import get_cleos, get_eos_rpc_client
 
 apps_to_test = ['app1', 'app2', 'app3']
 config = UnificationConfig()
@@ -18,8 +16,7 @@ def run_test(app):
     global conf
 
     print("Contacting MOTHER FOR: ", app)
-    eos_client = Client(
-        nodes=[f"http://{config['eos_rpc_ip']}:{config['eos_rpc_port']}"])
+    eos_client = get_eos_rpc_client()
     um = UnificationMother(eos_client, app, get_cleos())
     print("Valid app: ", um.valid_app())
     assert um.valid_app() is True
@@ -35,7 +32,8 @@ def run_test(app):
     assert um.get_haiku_rpc_ip() == demo_apps[app]['rpc_server']
 
     print("RPC Port: ", um.get_haiku_rpc_port())
-    assert int(um.get_haiku_rpc_port()) == int(demo_apps[app]['rpc_server_port'])
+    assert int(um.get_haiku_rpc_port()) == int(
+        demo_apps[app]['rpc_server_port'])
 
     print("RPC Server: ", um.get_haiku_rpc_server())
     print("-----------------------------------")
