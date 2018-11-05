580321e6cc61add972a26db2c6edca39baf0f834
Codegnosis <paul@codegnosis.co.uk>
2018-11-05 09:56:59 +0000

    verify update sig before adding to update obj

--- haiku_node/permissions/permissions.py
+++ haiku_node/permissions/permissions.py
@@ -1,6 +1,8 @@
 import json
 
 from haiku_node.blockchain.ipfs import IPFSDataStore
+from haiku_node.blockchain_helpers.eos.eos_keys import UnifEosKey
+from haiku_node.utils.utils import generate_perm_digest_sha
 
 
 class UnifPermissions:
@@ -9,6 +11,11 @@ class UnifPermissions:
         self.__consumer_perms = {}
         self.__updates = {}
         self.__ipfs = IPFSDataStore()
+
+    def __verify_update(self, perm_obj):
+        perm_digest_sha = generate_perm_digest_sha(perm_obj['perms'], perm_obj['schema_id'], perm_obj['p_nonce'], perm_obj['consumer'])
+        eosk = UnifEosKey()
+        return eosk.verify_pub_key(perm_obj['p_sig'], perm_digest_sha, perm_obj['pub_key'])
 
     def load_perms(self, ipfs_hash):
         perms_str = self.__ipfs.cat_file(ipfs_hash)
@@ -21,16 +28,17 @@ class UnifPermissions:
             return None
 
     def add_update(self, consumer, user, perms_obj):
-        if consumer not in self.__updates:
-            self.__updates[consumer] = {}
+        if self.__verify_update(perms_obj):
+            if consumer not in self.__updates:
+                self.__updates[consumer] = {}
 
-        if user not in self.__updates[consumer]:
-            self.__updates[consumer][user] = {}
+            if user not in self.__updates[consumer]:
+                self.__updates[consumer][user] = {}
 
-        if perms_obj['schema_id'] not in self.__updates[consumer][user]:
-            self.__updates[consumer][user][perms_obj['schema_id']] = {}
+            if perms_obj['schema_id'] not in self.__updates[consumer][user]:
+                self.__updates[consumer][user][perms_obj['schema_id']] = {}
 
-        self.__updates[consumer][user][perms_obj['schema_id']] = perms_obj
+            self.__updates[consumer][user][perms_obj['schema_id']] = perms_obj
 
     # def merge_updates(self):
     #     # Todo
