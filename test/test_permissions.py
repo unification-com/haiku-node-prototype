c7ceb06c92c959d75d8ec1c1cb7e2eeb68994578
Indika Piyasena <indika.piyasena@gmail.com>
2018-11-07 16:50:50 +0530

    test: test adding to the PermissonBatcher

--- test/test_permissions.py
+++ test/test_permissions.py
@@ -1,5 +1,21 @@
-import pytest
+import logging
+import tempfile
+
+from create_perm_batch_dbs import _create_perm_batch_db
+
+from haiku_node.permissions.permission_batcher import PermissionBatcher
+
+log = logging.getLogger(__name__)
 
 
-def test_permissions():
-    assert True
+def test_perm_batches():
+    f = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    log.info(f"Writing to {f.name}")
+
+    _create_perm_batch_db(f.name)
+    batch = PermissionBatcher(f.name)
+    batch.add_to_queue(
+        'user1', 'app1', 0, 'DataBlob', 2558268601041029,
+        'SIG_K1_Ka9y9vJzFCHeCxZr8zgV8h38p5tALjYxgdM874d2dnSRd3zeJCspMJJ65rb92V'
+        'xyo4XFPzaSFYW8TuN85HaHdS9deNCVnQ',
+        'EOS5z4FPxiDmptdPggLxApJQSzNkmsi6wYqNQnc8Y9exZVTcpusQw')
