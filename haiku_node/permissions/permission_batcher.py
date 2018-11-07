971881b44ba198cf76f7db830fae52f4e13e19d6
Indika Piyasena <indika.piyasena@gmail.com>
2018-11-07 18:03:45 +0530

    permissions: rename some vars

--- haiku_node/permissions/permission_batcher.py
+++ haiku_node/permissions/permission_batcher.py
@@ -10,15 +10,13 @@ from haiku_node.utils.utils import generate_nonce
 class PermissionBatcher:
 
     def __init__(self, permissions_db):
-        self.__pbdb = PermissionBatchDatabase(permissions_db)
+        self.db = PermissionBatchDatabase(permissions_db)
 
     def add_to_queue(self, *kwargs):
-        return self.__pbdb.add(*kwargs)
+        return self.db.add(*kwargs)
 
     def process_batch_queue(self, num=10):
         from haiku_node.permissions.permissions import UnifPermissions
-
-        batch = self.__pbdb.get_unprocessed(num)
 
         ipfs = get_ipfs_client()
         eos_client = get_eos_rpc_client()
@@ -29,24 +27,24 @@ class PermissionBatcher:
 
         processed = []
 
-        for b in batch:
-            perm_obj = {
-                'perms': b['perms'],
-                'p_nonce': b['p_nonce'],
-                'p_sig': b['p_sig'],
-                'pub_key': b['pub_key'],
-                'schema_id': b['schema_id'],
-                'consumer': b['consumer_account'],
-                'user': b['end_user_account'],
+        for item in self.db.get_unprocessed(num):
+            perm = {
+                'perms': item['perms'],
+                'p_nonce': item['p_nonce'],
+                'p_sig': item['p_sig'],
+                'pub_key': item['pub_key'],
+                'schema_id': item['schema_id'],
+                'consumer': item['consumer_account'],
+                'user': item['end_user_account'],
                 'b_nonce': generate_nonce(16),
                 'b_time': time.time()
             }
             is_added = permissions.add_change_request(
-                b['consumer_account'], b['end_user_account'], perm_obj)
+                item['consumer_account'], item['end_user_account'], perm)
 
             b_proc = {
-                'op_id': b['op_id'],
-                'consumer': b['consumer_account'],
+                'op_id': item['op_id'],
+                'consumer': item['consumer_account'],
                 'is_added': is_added
             }
 
@@ -54,30 +52,30 @@ class PermissionBatcher:
 
         ret_data = permissions.process_change_requests()
 
-        for b_p in processed:
-            if b_p['is_added']:
-                ret_d = ret_data[b_p['consumer']]
+        for item in processed:
+            if item['is_added']:
+                ret_d = ret_data[item['consumer']]
                 if ret_d['bc']:
-                    self.__pbdb.update_processed(
-                        b_p['op_id'], proof_tx=ret_d['proof_tx'])
+                    self.db.update_processed(
+                        item['op_id'], proof_tx=ret_d['proof_tx'])
 
                 else:
-                    stash_id = self.__pbdb.stash_permission(
+                    stash_id = self.db.stash_permission(
                         ret_d['stash']['consumer'],
                         ret_d['stash']['ipfs_hash'],
                         ret_d['stash']['merkle_root'])
 
-                    self.__pbdb.update_processed(b_p['op_id'],
-                                                 stash_id=stash_id)
+                    self.db.update_processed(item['op_id'],
+                                             stash_id=stash_id)
             else:
                 # Currently fails if sig is invalid, so delete
-                self.__pbdb.delete_op(b_p['op_id'])
+                self.db.delete_op(item['op_id'])
 
         # cleanup stashes
         for c, ret_d in ret_data.items():
             if ret_d['stash_id_committed'] is not None:
-                self.__pbdb.update_batch_stashes_with_tx(
+                self.db.update_batch_stashes_with_tx(
                     ret_d['stash_id_committed'], ret_d['proof_tx'])
-                self.__pbdb.delete_stash(ret_d['stash_id_committed'])
+                self.db.delete_stash(ret_d['stash_id_committed'])
 
         print(ret_data)
